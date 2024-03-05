92410e0236eaba8204d1f416a2d42b7312affd60
lubnariyaz <71705544+lubnariyaz@users.noreply.github.com>
2024-03-05 22:18:33 +0530

    Commit 5: Final Commit

--- employees/views.py
+++ employees/views.py
@@ -7,6 +7,8 @@ from employees.models import Employee
 
 def employee_detail(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
-    # return HttpResponse(employee)
-    return render(request, 'employee_detail.html')
+    context = {
+        'employee': employee,
+    }
+    return render(request, 'employee_detail.html', context)
     
