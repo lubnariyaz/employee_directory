3b16b62e98c2c4f0e673b6c3c58456e295daac0e
lubnariyaz <71705544+lubnariyaz@users.noreply.github.com>
2024-03-05 20:29:52 +0530

    Commit 4: Created media folder and fetched data from database onto home.html frontend

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from employees.models import Employee
 
 def home(request):
-    return render(request, 'home.html')
+    employees = Employee.objects.all()
+    # print(employees)
+    context = {
+        'employees': employees,
+    }
+    return render(request, 'home.html', context)
